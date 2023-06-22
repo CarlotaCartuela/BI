7bbc97f4f1cb66c5c4f7f2df3925b2aba422e239
CarlotaCartuela <137373285+CarlotaCartuela@users.noreply.github.com>
2023-06-22 03:53:27 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        RelatorioBI relatorioBI = new RelatorioBI();
+        relatorioBI.gerarRelatorio("caminho/para/contas_bancarias.json");
+    }
+}
+
+
+//preciso pegar as contas e por num arquivo em formato JASON caso não estejam assim, ainda falta essa parte para terminar :^)
