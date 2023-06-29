d815073c8bb10675d229a0fdf518c7929059ba34
CarlotaCartuela <137373285+CarlotaCartuela@users.noreply.github.com>
2023-06-29 03:16:47 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RelatorioBI relatorioBI = new RelatorioBI();
-        relatorioBI.gerarRelatorio("caminho/para/contas_bancarias.json");
-    }
-}
-
-
-//preciso pegar as contas e por num arquivo em formato JASON caso não estejam assim, ainda falta essa parte para terminar :^)
